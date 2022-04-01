f9cc144b63f8686dfbb8491a6b6443f3604fa336
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-04-01 13:47:57 +0300

    viewlog.html and function adding

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 """Creating a Flask Webapp Object"""
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def log_request(req: 'flask_request', res: str) -> None:
     """Function for logging information at the 'vsearch_log' file"""
     with open('vsearch.log', 'a') as vsearch_log:
-        print(req, res, file=vsearch_log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=vsearch_log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -30,8 +30,17 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome')
 
 
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    result_list = []
+    with open('vsearch.log') as log:
+        for line in log:
+            line = escape(line)
+            result_list.append(line.split('|'))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='View log', the_data=result_list, the_row_titles=titles,)
+
+
 if __name__ == '__main__':
     """Debugging mode"""
     app.run(debug=True)
-
-
