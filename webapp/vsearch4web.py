c28f31c9e5ea200161d613df7da5b851b4c7c4b4
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-04-11 16:21:52 +0300

    using s context management protocol

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,20 +1,16 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+from DBcm import UseDatabase
 
 """Creating a Flask Webapp Object"""
 app = Flask(__name__)
-
+app.config['dbconfig'] = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB', }
 
 def log_request(req: 'flask_request', res: str) -> None:
-    dbconfig = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB',}
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, 'TempleBrowser', res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    """Log details of the web request and the results."""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, 'TempleBrowser', res,))
 
 
 @app.route('/search4', methods=['POST'])
@@ -38,13 +34,12 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    result_list = []
-    with open('vsearch.log') as log:
-        for line in log:
-            line = escape(line)
-            result_list.append(line.split('|'))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html', the_title='View log', the_data=result_list, the_row_titles=titles,)
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='View log', the_data=contents, the_row_titles=titles, )
 
 
 if __name__ == '__main__':
